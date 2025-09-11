7ef1501ce3aaeb477ea1fa8f7b52ded538f42417
Leo Marek <56487792+leomarek@users.noreply.github.com>
2025-09-10 22:06:25 -0500

    add basic VLIW barebones

---
+++ riscv/riscv32_vliw_decoder.cc
@@ -0,0 +1,122 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "riscv/riscv32_vliw_decoder.h"
+
+#include <cstdint>
+#include <string>
+
+#include "absl/log/check.h"
+#include "absl/log/log.h"
+#include "mpact/sim/generic/instruction.h"
+#include "mpact/sim/generic/program_error.h"
+#include "mpact/sim/generic/type_helpers.h"
+#include "mpact/sim/util/memory/memory_interface.h"
+#include "riscv/riscv32_vliw_decoder.h"
+#include "riscv/riscv32_vliw_encoding.h"
+#include "riscv/riscv32g_enums.h"
+#include "riscv/riscv_state.h"
+
+namespace mpact {
+namespace sim {
+namespace riscv {
+
+using ::mpact::sim::generic::operator*;  // NOLINT: is used below (clang error).
+
+RiscV32Decoder::RiscV32Decoder(RiscVState* state, util::MemoryInterface* memory)
+    : state_(state), memory_(memory) {
+  // Get a handle to the internal error in the program error controller.
+  decode_error_ = state->program_error_controller()->GetProgramError(
+      generic::ProgramErrorController::kInternalErrorName);
+
+  // Need a data buffer to load instructions from memory. Allocate a single
+  // buffer that can be reused for each instruction word.
+  inst_db_ = state_->db_factory()->Allocate<uint32_t>(1);
+  // Allocate the isa factory class, the top level isa decoder instance, and
+  // the encoding parser.
+  riscv_isa_factory_ = new RV32IsaFactory();
+  riscv_isa_ = new isa32::RiscV32GInstructionSet(state, riscv_isa_factory_);
+  riscv_encoding_ = new isa32::RiscV32GEncoding(state);
+  decode_error_ = state->program_error_controller()->GetProgramError(
+      generic::ProgramErrorController::kInternalErrorName);
+}
+
+RiscV32Decoder::~RiscV32Decoder() {
+  delete riscv_isa_;
+  delete riscv_isa_factory_;
+  delete riscv_encoding_;
+  inst_db_->DecRef();
+}
+
+generic::Instruction* RiscV32Decoder::DecodeInstruction(uint64_t address) {
+  // First check that the address is aligned properly. If not, create and return
+  // an instruction object that will raise an exception.
+  if (address & 0x1) {
+    auto* inst = new generic::Instruction(0, state_);
+    inst->set_size(1);
+    inst->SetDisassemblyString("Misaligned instruction address");
+    inst->set_opcode(*isa32::OpcodeEnum::kNone);
+    inst->set_address(address);
+    inst->set_semantic_function([this](generic::Instruction* inst) {
+      state_->Trap(/*is_interrupt*/ false, inst->address(),
+                   *ExceptionCode::kInstructionAddressMisaligned,
+                   inst->address() ^ 0x1, inst);
+    });
+    return inst;
+  }
+
+  // If the address is greater than the max address, return an instruction
+  // that will raise an exception.
+  if (address > state_->max_physical_address()) {
+    auto* inst = new generic::Instruction(0, state_);
+    inst->set_size(0);
+    inst->SetDisassemblyString("Instruction access fault");
+    inst->set_opcode(*isa32::OpcodeEnum::kNone);
+    inst->set_address(address);
+    inst->set_semantic_function([this](generic::Instruction* inst) {
+      state_->Trap(/*is_interrupt*/ false, inst->address(),
+                   *ExceptionCode::kInstructionAccessFault, inst->address(),
+                   nullptr);
+    });
+    return inst;
+  }
+
+  // Read the instruction word from memory and parse it in the encoding parser.
+  memory_->Load(address, inst_db_, nullptr, nullptr);
+  uint32_t iword = inst_db_->Get<uint32_t>(0);
+  riscv_encoding_->ParseInstruction(iword);
+
+  // Call the isa decoder to obtain a new instruction object for the instruction
+  // word that was parsed above.
+  auto* instruction = riscv_isa_->Decode(address, riscv_encoding_);
+
+  // Search for VLIW START Hint Inst
+  LOG(INFO) << "opcode detected: " << instruction->opcode() << " name " << isa32::kOpcodeNames[instruction->opcode()];
+  if (instruction->opcode() == *isa32::OpcodeEnum::kCliHint) {
+    uint32_t vliw_inst = instruction->Source(1)->AsUint32(0);
+    LOG(INFO) << "HINT detected. Expecting " << vliw_inst << " VLIW inst.";
+
+    for (int i = 0; i < vliw_inst; i++) {
+      generic::Instruction *child = DecodeInstruction(address + 2 + 4 * i);
+      LOG(INFO) << " -> appending child opcode: " << child->opcode() << " name " << isa32::kOpcodeNames[child->opcode()];
+      instruction->AppendChild(child);
+    }
+    LOG(INFO) << "Returning parent inst:" << instruction->opcode() << " name " << isa32::kOpcodeNames[instruction->opcode()];
+  }
+  return instruction;
+}
+
+}  // namespace riscv
+}  // namespace sim
+}  // namespace mpact
---
+++ riscv/riscv32_vliw_decoder.h
@@ -0,0 +1,88 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef MPACT_RISCV_RISCV_RISCV32_DECODER_H_
+#define MPACT_RISCV_RISCV_RISCV32_DECODER_H_
+
+#include <cstdint>
+#include <memory>
+
+#include "mpact/sim/generic/data_buffer.h"
+#include "mpact/sim/generic/decoder_interface.h"
+#include "mpact/sim/generic/instruction.h"
+#include "mpact/sim/generic/program_error.h"
+#include "mpact/sim/generic/type_helpers.h"
+#include "mpact/sim/util/memory/memory_interface.h"
+#include "riscv/riscv32_vliw_decoder.h"
+#include "riscv/riscv32_vliw_encoding.h"
+#include "riscv/riscv32g_enums.h"
+#include "riscv/riscv_state.h"
+
+namespace mpact {
+namespace sim {
+namespace riscv {
+
+using ::mpact::sim::generic::operator*;  // NOLINT: clang-tidy false positive.
+
+// This is the factory class needed by the generated decoder. It is responsible
+// for creating the decoder for each slot instance. Since the riscv architecture
+// only has a single slot, it's a pretty simple class.
+class RV32IsaFactory : public isa32::RiscV32GInstructionSetFactory {
+ public:
+  std::unique_ptr<isa32::Riscv32gSlot> CreateRiscv32gSlot(
+      ArchState* state) override {
+    return std::make_unique<isa32::Riscv32gSlot>(state);
+  }
+};
+
+// This class implements the generic DecoderInterface and provides a bridge
+// to the (isa specific) generated decoder classes.
+class RiscV32Decoder : public generic::DecoderInterface {
+ public:
+  using SlotEnum = isa32::SlotEnum;
+  using OpcodeEnum = isa32::OpcodeEnum;
+
+  RiscV32Decoder(RiscVState* state, util::MemoryInterface* memory);
+  RiscV32Decoder() = delete;
+  ~RiscV32Decoder() override;
+
+  // This will always return a valid instruction that can be executed. In the
+  // case of a decode error, the semantic function in the instruction object
+  // instance will raise an internal simulator error when executed.
+  generic::Instruction* DecodeInstruction(uint64_t address) override;
+  // Return the number of opcodes supported by this decoder.
+  int GetNumOpcodes() const override { return *OpcodeEnum::kPastMaxValue; }
+  // Return the name of the opcode at the given index.
+  const char* GetOpcodeName(int index) const override {
+    return isa32::kOpcodeNames[index];
+  }
+
+  // Getter.
+  isa32::RiscV32GEncoding* riscv_encoding() const { return riscv_encoding_; }
+
+ private:
+  RiscVState* state_;
+  util::MemoryInterface* memory_;
+  std::unique_ptr<generic::ProgramError> decode_error_;
+  generic::DataBuffer* inst_db_;
+  isa32::RiscV32GEncoding* riscv_encoding_;
+  RV32IsaFactory* riscv_isa_factory_;
+  isa32::RiscV32GInstructionSet* riscv_isa_;
+};
+
+}  // namespace riscv
+}  // namespace sim
+}  // namespace mpact
+
+#endif  // MPACT_RISCV_RISCV_RISCV32_DECODER_H_
